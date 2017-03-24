ae9a9ad247ced8d41ea551f28f5e78e7fa92775c
wangqingsong <wangqs@cloud-young.com>
2017-03-24 10:35:22 +0800

    Initial Commit

---
+++ customTable/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  customTable
+//
+//  Created by hulinEasy on 2017/3/24.
+//  Copyright © 2017年 easy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ customTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  customTable
+//
+//  Created by hulinEasy on 2017/3/24.
+//  Copyright © 2017年 easy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
